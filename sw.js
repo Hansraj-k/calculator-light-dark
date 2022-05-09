cb62160df3c9ef8a57cc83b0b32bc809bd1ab8ef
Hansraj <hans182007@gmail.com>
2022-05-09 13:44:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 self.addEventListener("install", e => {
     console.log("Install dcode");
+});self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./","style.css","p404.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    console.log('Intercepting fetch request for: ${e.request.url}');
 });
